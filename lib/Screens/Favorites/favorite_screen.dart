43be55c2dbb9e965df0a014de4b33c809f03e8ea
HusseinMohamed <husseinhtm99@gmail.com>
2023-07-27 01:12:52 +0300

    Cart Screen ✔ Category Screen ✔ Category Details Screen ✔ Profile Screen ✔ Faqs Screen ✔ Settings Screen ✔ About Us Screen ✔ Change Password Screen ✔ Notifications Screen ✔ Order Screen ✔ Complaints Screen ✔ Favourite Screen ✔

--- lib/Screens/Favorites/favorite_screen.dart
+++ lib/Screens/Favorites/favorite_screen.dart
@@ -1,12 +1,15 @@
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:super_marko/generated/assets.dart';
 import 'package:super_marko/model/favorite/favorite_model.dart';
+import 'package:super_marko/shared/components/image_with_shimmer.dart';
 import 'package:super_marko/shared/components/my_divider.dart';
 import 'package:super_marko/shared/cubit/cubit.dart';
 import 'package:super_marko/shared/cubit/state.dart';
+import 'package:super_marko/shared/styles/colors.dart';
 import 'package:super_marko/shared/styles/icon_broken.dart';
 
 class FavoritesScreen extends StatelessWidget {
@@ -18,167 +21,47 @@ class FavoritesScreen extends StatelessWidget {
       listener: (context, state) {},
       builder: (context, state) {
         return Scaffold(
-            body: MainCubit.get(context).favoritesModel!.data!.data!.isEmpty
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(Assets.imagesNodata),
-                      Text(
-                        'Your Favorites is empty',
-                        style: Theme.of(context).textTheme.headlineSmall,
-                      ),
-                      Text(
-                        'Be sure to fill your favorite with something you like',
-                        style: Theme.of(context).textTheme.labelLarge,
-                      ),
-                    ],
-                  )
-                : ConditionalBuilder(
-                    condition: state is! FavoritesLoadingStates,
-                    builder: (context) => ListView.separated(
-                      physics: const BouncingScrollPhysics(),
-                      itemBuilder: (context, index) => ProductList(
-                        favoritesModel: MainCubit.get(context)
-                            .favoritesModel!
-                            .data!
-                            .data![index],
-                        // favoritesModel:
-                        //   MainCubit.get(context)
-                        //       .favoritesModel!
-                        //       .data!
-                        //       .data![index]
-                        //       .product,
-                      ),
-                      separatorBuilder: (context, index) => const MyDivider(),
-                      itemCount: MainCubit.get(context)
+          body: MainCubit.get(context).favoritesModel!.data!.data!.isEmpty
+              ? Column(
+                  children: [
+                    SvgPicture.asset(Assets.imagesNodata),
+                    Text(
+                      'Your Favorites is empty',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                    Text(
+                      'Be sure to fill your favorite with something you like',
+                      style: Theme.of(context).textTheme.labelLarge,
+                    ),
+                  ],
+                )
+              : ConditionalBuilder(
+                  condition: state is! FavoritesLoadingStates,
+                  builder: (context) => ListView.separated(
+                    physics: const BouncingScrollPhysics(),
+                    itemBuilder: (context, index) => ProductList(
+                      favoritesModel: MainCubit.get(context)
                           .favoritesModel!
                           .data!
-                          .data!
-                          .length,
+                          .data![index],
                     ),
-                    fallback: (context) =>
-                        const Center(child: CircularProgressIndicator()),
-                  ));
+                    separatorBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.all(8.0).r,
+                      child: const MyDivider(),
+                    ),
+                    itemCount: MainCubit.get(context)
+                        .favoritesModel!
+                        .data!
+                        .data!
+                        .length,
+                  ),
+                  fallback: (context) =>
+                      const Center(child: CircularProgressIndicator()),
+                ),
+        );
       },
     );
   }
-
-// Widget buildListProduct(
-//   model,
-//   context, {
-//   isOldPrice = true,
-// }) =>
-//     Padding(
-//       padding: const EdgeInsets.all(20.0),
-//       child: SizedBox(
-//         height: 400.0,
-//         child: Column(
-//           children: [
-//             Stack(
-//               alignment: AlignmentDirectional.bottomEnd,
-//               children: [
-//                 Image(
-//                   image: NetworkImage(
-//                     model.image,
-//                   ),
-//                   height: 250.0,
-//                   width: double.infinity,
-//                 ),
-//                 if (model.discount != 0 && isOldPrice)
-//                   Container(
-//                     padding: const EdgeInsets.symmetric(
-//                       horizontal: 5.0,
-//                     ),
-//                     color: Colors.red,
-//                     child: const Text(
-//                       'OFFERS',
-//                       style: TextStyle(
-//                         color: Colors.white,
-//                         fontSize: 10.0,
-//                       ),
-//                     ),
-//                   ),
-//                 Positioned(
-//                   bottom: 10,
-//                   left: 10,
-//                   child: CircleAvatar(
-//                     backgroundColor: MainCubit.get(context).cart[model.id]
-//                         ? Colors.deepOrangeAccent
-//                         : Colors.grey[300],
-//                     child: IconButton(
-//                       onPressed: () {
-//                         MainCubit.get(context).changeCart(model.id);
-//                       },
-//                       icon: const Icon(
-//                         Icons.add_shopping_cart,
-//                         color: Colors.white,
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ],
-//             ),
-//             const SizedBox(
-//               width: 20.0,
-//             ),
-//             Expanded(
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Padding(
-//                     padding: const EdgeInsets.symmetric(vertical: 20.0),
-//                     child: Text(
-//                       model.name,
-//                       maxLines: 2,
-//                       overflow: TextOverflow.ellipsis,
-//                       style: const TextStyle(height: 1.5),
-//                     ),
-//                   ),
-//                   const Spacer(),
-//                   Row(
-//                     children: [
-//                       Text(
-//                         '${model.price.round()}',
-//                         style: const TextStyle(
-//                           color: Colors.deepOrange,
-//                         ),
-//                       ),
-//                       const SizedBox(
-//                         width: 10.0,
-//                       ),
-//                       if (model.discount != 0 && isOldPrice)
-//                         Text(
-//                           '${model.oldPrice.round()}',
-//                           style: const TextStyle(
-//                             color: Colors.grey,
-//                             decoration: TextDecoration.lineThrough,
-//                           ),
-//                         ),
-//                       const Spacer(),
-//                       CircleAvatar(
-//                         backgroundColor:
-//                             MainCubit.get(context).favorites[model.id]
-//                                 ? Colors.red
-//                                 : Colors.grey[300],
-//                         child: IconButton(
-//                           onPressed: () {
-//                             MainCubit.get(context).changeFavorites(model.id);
-//                           },
-//                           icon: const Icon(
-//                             IconBroken.Delete,
-//                             color: Colors.white,
-//                           ),
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
 }
 
 class ProductList extends StatelessWidget {
@@ -190,110 +73,88 @@ class ProductList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20).r,
       child: SizedBox(
-        height: 400.0,
+        height: 320.h,
         child: Column(
           children: [
             Stack(
-              alignment: AlignmentDirectional.bottomEnd,
               children: [
-                Image(
-                  image: NetworkImage(
-                    favoritesModel.product!.image!,
-                  ),
-                  height: 250.0,
+                ImageWithShimmer(
+                  imageUrl: favoritesModel.product!.image!,
+                  height: 250.h,
                   width: double.infinity,
                 ),
                 if (favoritesModel.product!.discount != 0 && isOldPrice)
-                  Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 5.0,
-                    ),
-                    color: Colors.red,
-                    child: const Text(
-                      'OFFERS',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 10.0,
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ).r,
+                      color: AppMainColors.redColor,
+                      child: Text(
+                        'OFFERS',
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleSmall!
+                            .copyWith(color: AppMainColors.whiteColor),
                       ),
                     ),
                   ),
-                Positioned(
-                  bottom: 10,
-                  left: 10,
+                Align(
+                  alignment: Alignment.topRight,
                   child: CircleAvatar(
-                    backgroundColor:
-                        MainCubit.get(context).cart[favoritesModel.product!.id]
-                            ? Colors.deepOrangeAccent
-                            : Colors.grey[300],
+                    radius: 20.r,
+                    backgroundColor: MainCubit.get(context)
+                            .favorites[favoritesModel.product!.id]
+                        ? Colors.red
+                        : Colors.grey[300],
                     child: IconButton(
                       onPressed: () {
                         MainCubit.get(context)
-                            .changeCart(favoritesModel.product!.id!);
+                            .changeFavorites(favoritesModel.product!.id!);
                       },
-                      icon: const Icon(
-                        Icons.add_shopping_cart,
-                        color: Colors.white,
+                      icon: Icon(
+                        IconBroken.Heart,
+                        color: AppMainColors.whiteColor,
+                        size: 24.sp,
                       ),
                     ),
                   ),
                 ),
               ],
             ),
-            const SizedBox(
-              width: 20.0,
-            ),
             Expanded(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 20.0),
-                    child: Text(
-                      favoritesModel.product!.name!,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(height: 1.5),
-                    ),
+                  Text(
+                    favoritesModel.product!.name!,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.titleMedium,
                   ),
-                  const Spacer(),
+                  SizedBox(height: 20.h),
                   Row(
                     children: [
                       Text(
                         '${favoritesModel.product!.price.round()}',
-                        style: const TextStyle(
-                          color: Colors.deepOrange,
-                        ),
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium!
+                            .copyWith(color: AppMainColors.redColor),
                       ),
-                      const SizedBox(
-                        width: 10.0,
-                      ),
+                      SizedBox(width: 20.w),
                       if (favoritesModel.product!.discount != 0 && isOldPrice)
                         Text(
                           '${favoritesModel.product!.oldPrice.round()}',
-                          style: const TextStyle(
-                            color: Colors.grey,
-                            decoration: TextDecoration.lineThrough,
-                          ),
+                          style:
+                              Theme.of(context).textTheme.titleMedium!.copyWith(
+                                    color: AppMainColors.greyColor,
+                                    decoration: TextDecoration.lineThrough,
+                                  ),
                         ),
-                      const Spacer(),
-                      CircleAvatar(
-                        backgroundColor: MainCubit.get(context)
-                                .favorites[favoritesModel.product!.id]
-                            ? Colors.red
-                            : Colors.grey[300],
-                        child: IconButton(
-                          onPressed: () {
-                            MainCubit.get(context)
-                                .changeFavorites(favoritesModel.product!.id!);
-                          },
-                          icon: const Icon(
-                            IconBroken.Delete,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
                     ],
                   ),
                 ],
--- lib/shared/components/my_divider.dart
+++ lib/shared/components/my_divider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:super_marko/shared/styles/colors.dart';
 
 class MyDivider extends StatelessWidget {
   const MyDivider({super.key});
@@ -9,7 +10,7 @@ class MyDivider extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: 1.h,
-      color: Colors.grey[300],
+      color: AppMainColors.dividerColor,
     );
   }
 }
